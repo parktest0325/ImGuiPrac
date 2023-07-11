516c9dfa1847b90c3b929efc3d742b0764ff0876
dongkim <parktest0325@gmail.com>
2023-07-11 16:39:05 +0900

    Feat: WallClock Own

---
+++ 2_ImGui/WallClock_Own/src/render.hpp
@@ -0,0 +1,52 @@
+#pragma once
+
+#include <cstdint>
+#include <string_view>
+#include <numbers>
+#include <tuple>
+
+class WindowClass
+{
+private:
+    static constexpr auto PI = std::numbers::pi_v<float>;
+    static constexpr auto circleRadius = 250.0f;
+    static constexpr auto offset = PI / 2.0f;
+    static constexpr auto innerRadius = 5.0f;
+
+    static constexpr auto hrsClockHandLength = 0.75F;
+    static constexpr auto minsClockHandLength = 0.85F;
+    static constexpr auto secsClockHandLength = 0.95F;
+
+    static constexpr auto hrsStrokesLength = 0.90f;
+    static constexpr auto minsStrokesLength = 0.95f;
+    static constexpr auto secsStrokesLength = 1.0f;
+
+
+public:
+    WindowClass() : secs(0), mins(0), hrs(0), center({}){};
+    void Draw(std::string_view label);
+
+private:
+    void DrawCircle(const float radius);
+    void DrawClockHand(const float radius,
+                       const float theta,
+                       const ImColor color);
+    void DrawAllHourStrokes();
+    void DrawAllMinuteStrokes();
+    void DrawDigitalClock();
+
+    void GetTime();
+    std::tuple<float, float, float> GetTheta();
+
+public:
+    std::int32_t secs;
+    std::int32_t mins;
+    std::int32_t hrs;
+
+private:
+    ImVec2 center;
+
+
+};
+
+void render(WindowClass &window_obj);
